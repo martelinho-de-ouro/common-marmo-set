67fa67c089b8b5259b8a7e3459b50d965dcce613
Helio Frota <00hf11@gmail.com>
2023-11-06 15:26:23 -0300

    some pub and mod basic things

--- src/modpub/modpub.rs
+++ src/modpub/modpub.rs
@@ -4,6 +4,23 @@
 // *  `src/lib.rs` is the crate root for library crate, by convention. 
 // * A package can have multi binary crates by placing files in the src/bin dir.
 //   * Each file will be a separate binary crate.
+// * The compiler tries to find a module in these places:
+//   * Inline, within curly brackets that replace the semicolon following `mod foo`
+//   * In the file src/foo.rs
+//   * In the file src/foo/mod.rs
+//     * In the case of a submodule called `bar`
+//       * Inline, directly following mod bar, within curly brackets instead of the semicolon
+//       * In the file src/foo/bar.rs
+//       * In the file src/foo/bar/mod.rs
+// * A BarFoo type inside foo-bar module would be found at
+//   * crate::foo::bar::BarFoo
+// * `pub` makes the thing (module and items) public, without `pub` the thing is private.
+
+use crate::foo::bar::FooBar;
+
+pub mod foo;
+
 fn main() {
-    println!("aaaaa");
+    let foobar = FooBar {};
+    println!("result: {:?}", foobar);
 }
