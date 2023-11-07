e9cdd1ab8eda4c8601bb1bfe1f039ac82e15f36d
Helio Frota <00hf11@gmail.com>
2023-11-07 09:11:15 -0300

    basic collections

--- src/bin/coll.rs
+++ src/bin/coll.rs
@@ -33,6 +33,43 @@ fn main() {
     let hold_my_beer = &v1[0];
     v1.push(9);
     // We can't do that at the same time.
+    // "cannot borrow `v1` as mutable because it is also borrowed as immutable"
+    // ------
+    // vectors put the values next to each other in memory, 
+    // adding a new element onto the end of the vector might require allocating 
+    // new memory and copying the old elements to the new space, 
+    // if there isn’t enough room to put all the elements next to each other 
+    // where the vector is currently stored. 
+    // In that case, the reference to the first element would be pointing 
+    // to deallocated memory. The borrowing rules prevent programs 
+    // from ending up in that situation
+    // ------
     // println!("{:?}", hold_my_beer);
+
+    for i in &v1 {
+        println!("{i}");
+    }
+
+    for j in &mut v1 {
+        // * dereference operator
+        *j += 1;
+        println!("{j}");
+        
+        // not possible to add or delete values inside for loop
+        // v1.push(0);
+        // v1.remove(0);
+    }
+
+    // We can use enum + vectors to store different types
+    enum Car {
+        Name(String),
+        Year(i16),
+    }
     
+    let cars  = vec![Car::Name(String::from("foo")), Car::Year(1983)];
+
+    {
+        let v3 = vec![1,1,2];
+    } // after this the memory is cleaned up.
+
 }
